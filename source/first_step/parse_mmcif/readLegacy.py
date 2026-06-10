0468eee8671ac1bd6dd01c79bfce04560fe679c2
Jessie <jessie@pdb-vm-linux-055.rcsb.rutgers.edu>
2026-06-10 11:38:14 -0400

    structure

---
+++ source/first_step/parse_mmcif/readLegacy.py
@@ -0,0 +1,138 @@
+# --- for server use ----
+
+import logging
+import os
+import sys
+from mmcif.io.IoAdapterCore import IoAdapterCore
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+SRC_DIR = os.path.dirname(os.path.dirname(DIR))
+UTIL_DIR = os.path.join(SRC_DIR, "util")
+PROJECT_DIR = os.path.dirname(SRC_DIR) 
+DATA_DIR = os.path.join(PROJECT_DIR, "data") #data directory
+if not os.path.isdir(DATA_DIR): # makes one if it does not exist
+    os.makedirs(DATA_DIR)
+
+
+sys.path.insert(0, UTIL_DIR)
+from convertCatDataFormat import convertCatObjToDict
+
+## ^ ask
+
+# ------------------------
+# place holder to a class to read legacy? the mmcif data files and extract specific information.
+
+logger = logging.getLogger(__name__)
+
+
+class LegacyReader:
+    """A class to read legacy data files and extract specific information.
+    This class reads a file, extracts the 'audit_contact_author' category, 
+    and 'citation' category primary publication's pubmed id and doi.
+    It stores the extracted data in a dictionary LegacyReader.d_.
+    Attributes:
+        filepath (str): The path to the legacy data file.
+        l_dc (list): List of data categories read from the file.
+        dc0 (object): The first data category object.
+        d_ (dict): Dictionary to store extracted data.
+    Methods:
+        __init__(filepath): Initializes the reader with the file path.
+        readCategory(): Reads the 'audit_contact_author' category and updates the dictionary.
+        cleanDict(): Cleans the dictionary by removing tab and return char.
+    """
+    def __init__(self, filepath):
+        if not os.path.isfile(filepath):
+            logger.error("failed to find file at %s", filepath)
+            sys.exit(1)
+            
+        logger.info("start to read %s", filepath)
+        self.io = IoAdapterCore()
+        self.l_dc = self.io.readFile(filepath)
+        self.dc0 = self.l_dc[0]
+        logger.info("finished reading %s", filepath)
+        
+        self.d_category = {}
+
+        
+    def readCategory(self, category):
+        """a method to read a category from the legacy data file.
+        This method checks if the  category exists in the data categories.
+        If it exists, it converts the category object to a dictionary and updates the instance's dictionary.
+        
+        Returns:
+            bool: True if the category was read successfully, False otherwise.
+        """
+        if category not in self.dc0.getObjNameList():
+            logger.error("failed to find category") 
+            return False
+        
+        if type(category) is not str:
+            logger.error("category not in string format") 
+            return False
+
+        try:
+            c_cat = self.dc0.getObj(category)
+            self.d_category = convertCatObjToDict(c_cat)  # use convertCatObjToDict utility to convert category object to dictionary
+            return True
+        except Exception as e:
+            logger.error("failed to read category with error %s", e)
+            return False
+
+        
+    # def readCitationPrimary(self):
+    #      """a method to read the citation category from the legacy data file.
+    #     This method checks if the 'citation' category exists in the data categories.
+    #     If it exists, it converts two category objects (pdbx_database_id_PubMed and .pdbx_database_id_DOI) to a dictionary and updates the instance's dictionary.
+        
+    #     Returns:
+    #         bool: True if the category was read successfully, False otherwise.
+    #      """
+    #      d_new = {}
+
+
+    #      if "citation" not in self.dc0.getObjNameList():
+    #          logger.error("failed to find citation")
+    #          return False
+
+    #      try:
+    #          c_cat = self.dc0.getObj("citation")
+    #          d_cat = convertCatObjToDict(c_cat)
+    #          for i in range(len(list(d_cat.values())[0])):
+    #              if d_cat["_citation.id"][i].lower() == "primary":
+    #                  self.d_citation["_citation.pdbx_database_id_PubMed"] = [d_cat["_citation.pdbx_database_id_PubMed"][i]]
+    #                  self.d_citation["_citation.pdbx_database_id_DOI"] = [d_cat["_citation.pdbx_database_id_DOI"][i]]
+    #                  break
+    
+    #          logger.info(f"d_citation is now {self.d_citation}")
+    #          return True
+            
+    #      except Exception as e:
+    #          logger.error("failed to read citation with error %s", e)
+    #          return False
+        
+
+    def cleanOne(self, input):
+        return ' '.join(str(input).split())
+
+         
+    def cleanDictOne(self, d_one):
+         """a method to clean the dictionary of extracted data by removing special characters that could cause errors
+        
+        Returns:
+            bool: True if the category was read successfully, False otherwise.
+         """
+         try:
+             for key, value in d_one.items():
+                 for i in range(len(list(value))):
+                     d_one[key][i] = self.cleanOne(value[i])        
+             return True
+
+         except Exception as e:
+             logger.error("failed to clean dictionary with error %s", e)
+             return False
+
+    def cleanDict(self):
+        if self.d_contact:
+            self.cleanDictOne(self.d_contact)
+        if self.d_citation:
+            self.cleanDictOne(self.d_citation)
---
+++ source/first_step/parse_mmcif/readRcsb.py
@@ -0,0 +1,130 @@
+import csv
+import os
+import sys
+
+from concurrent.futures import ProcessPoolExecutor
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+SRC_DIR = os.path.dirname(os.path.dirname(DIR))
+UTIL_DIR = os.path.join(SRC_DIR, "util")
+PROJECT_DIR = os.path.dirname(SRC_DIR) 
+DATA_DIR = os.path.join(PROJECT_DIR, "data") #data directory
+if not os.path.isdir(DATA_DIR): # makes one if it does not exist
+    os.makedirs(DATA_DIR)
+
+
+sys.path.insert(0, PROJECT_DIR)
+from first_step.parse_mmcif.readLegacyOne import LegacyReader
+
+# sys.path.insert(0, UTIL_DIR)
+# from pathFinderNew import PathFinderWwpdbLocal, PathFinderWwpdbLegacy
+
+#----------logging-----------
+import logging
+# logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+# logger = logging.getLogger(__name__)
+log_format = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(module)s - %(funcName)s:%(lineno)d - %(message)s')
+f_handler = logging.FileHandler(os.path.join(LOG_DIR, "readRcsbAll.log"), mode='w', encoding='utf-8')
+f_handler.setLevel(logging.DEBUG)
+f_handler.setFormatter(log_format)
+
+c_handler = logging.StreamHandler()
+c_handler.setLevel(logging.DEBUG)
+c_handler.setFormatter(log_format)
+
+logger = logging.getLogger("")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(f_handler)
+logger.addHandler(c_handler)
+#-----------------------------
+
+
+def writeDictToFile(d_all, fp, l_item):
+    """a method to write dictionary information into a tsv (tab separated values) file, given a dictionary, a filepath, and a list of headings.
+        
+        Returns:
+            bool: True if the category was read successfully, False otherwise.
+    """
+    l_h = ["id"]
+    l_h.extend(l_item) # add rest of headings
+    
+    with open(fp, 'w') as f:
+        f.write("\t".join(l_h)) #separate headings w tabs
+        f.write("\n") # start adding data on a new line
+        for id, d_one in d_all.items(): # for each id, take one of the dictionaries (contact/citation)
+            if d_one:
+                for i in range(len(list(d_one.values())[0])): # for every value in the dictionary
+                    l_line = [id] # new line staring w id
+                    for item in l_item: # item (not category)
+                        l_line.append(d_one[item][i]) # add all contact info (items only)
+                    f.write("\t".join(l_line)) # combine into a line w a tab separation
+                    f.write("\n") # new line for  new data
+            else:
+                logger.warning("entry %s has EMPTY dict", id)
+                continue
+    return True
+
+def workerOne(id, category):
+    fp = os.path.join(DATA_DIR, id, ".cif")
+
+    logger.info("filepath at %s", fp)
+    reader = LegacyReader(fp)
+    reader.readCategory(category) # read category, save to dictionary
+    reader.cleanDict() #clean
+    rt_data = reader.d_contact # return a dictionary
+
+    return rt_data
+
+def processList(l_id, category):
+    """a method to take a list of data files and extracts information from each file  of thelist into two dictionaries: one for  audit contact information and one for  primary citation information. Then, it converts each dictionary into a tsv file.
+    
+    Returns:
+        none.
+    """
+    data_folder = "/Users/jessiewang/data"
+    # data_folder = "/Users/chenghua/Projects/Training/data"
+
+    l_id_test = l_id
+
+    with ProcessPoolExecutor() as executor:
+        results = executor.map(workerOne, l_id_test, category)
+    # map returns a generator, so convert to list if needed
+    results_list = list(results)
+
+    d_category_all = {}
+
+    for i in range(len(l_id_test)):
+        try:
+            id = l_id[i]
+            d_category = results_list[i]
+            logger.info(f"Processing {id} with category {d_category}")    
+            d_category[id] = d_category # for a key [the id], add category info
+            
+        except IndexError as e:
+            logger.error("entry %s with error %s", id, e)
+            continue
+
+    fn_category = category + ".tsv"
+    fp_category = os.path.join(DATA_DIR, "parse_mmcif", fn_category)
+
+    l_item_contact = ['_audit_contact_author.name', '_audit_contact_author.address', '_audit_contact_author.phone', '_audit_contact_author.fax', '_audit_contact_author.email']
+    writeDictToFile(d_categroy_all, fp_category, l_item_contact)
+        
+
+def main():
+    #fn_list = "test_id.list"
+    # fn_list = "yes_audit_contact_author.list"
+    # fp_list = os.path.join(DATA_DIR, "parse_mmcif", fn_list)
+
+    # with open(fp_list) as f:
+    #     l_id = f.read().splitlines() # list of ids, split file at a new line 
+
+    # # logger.info(l_id)
+    category = "_struct_keywords"
+    id = "D_1001407944"
+    workerOne(id, category)
+    #processList(l_id, category)    
+
+if __name__ == "__main__":
+    main()
