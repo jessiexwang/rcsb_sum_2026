e8959bc43781daea0c6b0d6559d9941eec41ee0b
jxwang-10 <jxwang@smith.edu>
2026-06-11 14:55:13 -0400

    test processlist category15

--- source/first_step/parse_mmcif/readRcsb.py
+++ source/first_step/parse_mmcif/readRcsb.py
@@ -138,13 +138,13 @@ def main():
 
     category = 'exptl_crystal_grow'
     category2 = 'source_diffrn'
-    l_category = ['exptl_crystal_grow', 'diffrn_radiation_wavelength', 'source_diffrn']
+    l_category = ['exptl_crystal_grow', 'diffrn_radiation_wavelength', 'diffrn_source']
     id = "D_1001407944"
     l_id = ["D_1001407944", "D_1001407945"]
     l_struct_keywords = ['_struct_keywords.entry_id', '_struct_keywords.pdbx_keywords', '_struct_keywords.text']
     l_crystal_grow = ['_exptl_crystal_grow.temp', '_exptl_crystal_grow.method']
     l_radiation_wl = ['_diffrn_radiation_wavelength.wavelength']
-    l_diffrn_source = ['_source_diffrn.type']
+    l_diffrn_source = ['_diffrn_source.type']
     l_item_cat = [l_crystal_grow, l_radiation_wl, l_diffrn_source]
 
     #processList_id(l_id, category2, l_diffrn_source)
