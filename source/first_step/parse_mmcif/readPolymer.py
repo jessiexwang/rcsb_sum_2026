6a0a0ed64c89a915c7d248b3fa2094ffa6118190
jxwang-10 <jxwang@smith.edu>
2026-06-17 15:26:03 -0400

    whoops2

--- source/first_step/parse_mmcif/readPolymer.py
+++ source/first_step/parse_mmcif/readPolymer.py
@@ -50,7 +50,7 @@ class readPolymer():
                          "_struct_ref.db_code", "entity", "entity_poly","struct_ref", "entity_src_gen"]
         self.list = []
 
-    def writeDictToFile(d_all, fp, l_item):
+    def writeDictToFile(self, d_all, fp, l_item):
         """a method to write dictionary information into a tsv (tab separated values) file, given a dictionary, a filepath, and a list of headings.
         
         Returns:
