99efa375da246a93c6e752d62cfc89a07115e9ff
jxwang-10 <jxwang@smith.edu>
2026-06-17 15:34:51 -0400

    test readPolymer error

--- source/first_step/parse_mmcif/readSingle.py
+++ source/first_step/parse_mmcif/readSingle.py
@@ -59,6 +59,7 @@ def writeDictToFile(d_all, fp, l_item):
         f.write("\n") # start adding data on a new line
         for id, d_one in d_all.items(): # for each id, take one of the dictionaries (contact/citation)
             if d_one:
+                print(len(list(d_one.values())[0]))
                 for i in range(len(list(d_one.values())[0])): # for every value in the dictionary
                     l_line = [id] # new line staring w id
                     for item in l_item: # item (not category)
