78add53f09a6f00a4b1f858d48f8b5f2f0aad03c
jxwang-10 <jxwang@smith.edu>
2026-06-17 19:23:23 -0400

    test deleting 5

--- source/first_step/parse_mmcif/readPolymer.py
+++ source/first_step/parse_mmcif/readPolymer.py
@@ -84,6 +84,7 @@ class readPolymer():
             print(en_type)
             if en_type[i] != "polymer":
                 for item in d1:
+                    print(d1[item][i])
                     del d1[item][i]
         
 
