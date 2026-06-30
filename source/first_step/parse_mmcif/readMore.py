de169a7049720f5dea30c2d3ea23405485bede1a
jxwang-10 <jxwang@smith.edu>
2026-06-30 16:11:14 -0400

    test read combine than 2 categories

--- source/first_step/parse_mmcif/readMore.py
+++ source/first_step/parse_mmcif/readMore.py
@@ -19,7 +19,6 @@ if not os.path.isdir(LOG_DIR):
 
 sys.path.insert(0, SRC_DIR)
 from first_step.parse_mmcif.readSingle import workerOne_all
-from first_step.parse_mmcif.readSingle import writeDictToFile
 
 #----------logging-----------
 import logging
@@ -41,41 +40,41 @@ logger.addHandler(f_handler)
 logger.addHandler(c_handler)
 #-----------------------------
 
-class readTwo:
-    """a class to read mmcif data files and extract certain metadata from two groups about the entry's structure assembly
+class readMore:
+    """a class to read mmcif data files and extract certain metadata from any amount of groups about the entry's structure assembly
     Attributes:
         list: list to combine dictionaries
         l_cat: list of select attributes
     """
     def __init__(self):
         self.list = []
-        self.l_cat = []
+        self.l_cat_attr = []
 
-
-    def readTwoCat(self, group, cat1, cat2, id):
+    def readMoreCat(self, group, l_cat, id):
         """method to read the two categories+ their attributes needed
 
         Args:
             group (str): group that the entry belongs to
             id (str): dep id
-            cat1 (str): first category to parse
-            cat2 (str): second category to parse
+            l_cat (str): list of categories to parse
 
         Returns:
             dict: dictionary of combined parsed info
         """
-        d1 = workerOne_all(cat1, group, id)
-        d2 = workerOne_all(cat2, group, id)
+        dict_all = {}
 
-        d1.update(d2)
+        for category in l_cat:
+            d1 = workerOne_all(category, group, id)
+            dict_all.update(d1)
+        
         d_new = {}
 
-        for item in self.l_cat:
-            d_new[item] = d1[item]
+        for item in self.l_cat_attr:
+            d_new[item] = dict_all[item]
 
         return d_new
     
-    def readTwo(self, group, cat1, cat2, l_id, l_cat, file_name, num):
+    def readMore(self, group, l_cat, l_id, l_cat_attr, file_name):
         """method to parse out all the needed information about an entry
 
         Args:
@@ -86,9 +85,9 @@ class readTwo:
             l_cat (str): list of attributes to parse
             file_name (str): name of file out
         """
-        self.l_cat = l_cat.copy()
+        self.l_cat_attr = l_cat_attr.copy()
 
-        partial_readTwoCat = functools.partial(self.readTwoCat, group, cat1, cat2)
+        partial_readTwoCat = functools.partial(self.readMoreCat, group, l_cat)
 
         with ProcessPoolExecutor() as executor:
             results = executor.map(partial_readTwoCat, l_id)
@@ -111,7 +110,7 @@ class readTwo:
 
         # fn = file_name +".tsv"
         # fp = os.path.join(DATA_DIR, "parse_mmcif", fn)
-        fn_json = file_name + num +".json"
+        fn_json = file_name + ".json"
         fp_category_json = os.path.join(DATA_DIR, "parse_mmcif", fn_json)
 
 
@@ -121,14 +120,15 @@ class readTwo:
 def main():
    l_id = ["D_1001407944", "D_1001407945", "D_1001407946"]
    group = 'G_1002329'
+   l_cat = ["citation", "citation_author", "struct"]
    cat1 = "citation"
    cat2 = "citation_author"
    #l_cat = ["_audit_author.name", "_audit_author.pdbx_ordinal", "_struct.title"]
-   l_cat = ["_citation.title", "_citation_author.name"]
-   fn = "citation"
+   l_attr = ["_citation.title", "_citation_author.name", "_struct.title"]
+   fn = "test"
 
-   rt = readTwo()
-   rt.readTwo(group, cat1, cat2, l_id, l_cat, fn)
+   rm= readMore()
+   rm.readMore(group, l_cat, l_id, l_attr, fn)
 
 if __name__ == "__main__":
     main()
